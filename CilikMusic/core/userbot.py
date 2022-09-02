43d101de2b6a95478bf61e3ac8b20d9b2ae3e550
benkorsan <103997850+benkorsan@users.noreply.github.com>
2022-09-02 22:42:20 +0300

    Update userbot.py

--- CilikMusic/core/userbot.py
+++ CilikMusic/core/userbot.py
@@ -57,9 +57,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("CilikProject")
-                await self.one.join_chat("botlogsid")
-                await self.one.join_chat("CilikSupport")
+                await self.one.join_chat("CanliSohbetGruplari")
+                await self.one.join_chat("CanliSohbetGruplari")
+                await self.one.join_chat("CanliSohbetGruplari")
             except:
                 pass
             assistants.append(1)
@@ -88,9 +88,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("CilikProject")
-                await self.two.join_chat("botlogsid")
-                await self.two.join_chat("CilikSupport")
+                await self.two.join_chat("CanliSohbetGruplari")
+                await self.two.join_chat("CanliSohbetGruplari")
+                await self.two.join_chat("CanliSohbetGruplari")
             except:
                 pass
             assistants.append(2)
@@ -119,9 +119,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("CilikProject")
-                await self.three.join_chat("botlogsid")
-                await self.three.join_chat("CilikSupport")
+                await self.three.join_chat("CanliSohbetGruplari")
+                await self.three.join_chat("CanliSohbetGruplari")
+                await self.three.join_chat("CanliSohbetGruplari")
             except:
                 pass
             assistants.append(3)
@@ -150,9 +150,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("CilikProject")
-                await self.four.join_chat("botlogsid")
-                await self.four.join_chat("CilikSupport")
+                await self.four.join_chat("CanliSohbetGruplari")
+                await self.four.join_chat("CanliSohbetGruplari")
+                await self.four.join_chat("CanliSohbetGruplari")
             except:
                 pass
             assistants.append(4)
@@ -181,9 +181,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("CilikProject")
-                await self.five.join_chat("botlogsid")
-                await self.five.join_chat("CilikSupport")
+                await self.five.join_chat("CanliSohbetGruplari")
+                await self.five.join_chat("CanliSohbetGruplari")
+                await self.five.join_chat("CanliSohbetGruplari")
             except:
                 pass
             assistants.append(5)
